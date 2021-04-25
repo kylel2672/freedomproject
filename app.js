d988a46fe2423ddebb60a4c37e3a6a3aca43daf1
Kyle Lin <kylel2672@hstat.org>
2021-04-25 20:07:08 +0000

    update repo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", event =>{
+    const app = firebase.app();
+
+})
+
+function googleLogin(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    
+    .then(result => {
+        const user = result.user;
+        document.write("hello" + $user.displayName)
+        console.log(user)
+    })
+    .catch(console.log)
+}
